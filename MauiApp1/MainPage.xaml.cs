83b32832f4d4cccfd63d3f6bbbadc1c57bb4381c
Martin Rothschink <martin.rothschink@axonet.de>
2022-12-05 17:56:33 +0100

    Projektdateien hinzufügen.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+    }
+
+    private void ButtonStart_Pressed(object sender, EventArgs e)
+    {
+        Label.Text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.";
+    }
+
+    private void ButtonStop_Pressed(object sender, EventArgs e)
+    {
+        Label.Text = "Short text";
+    }
+}
+
